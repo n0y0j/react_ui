30c1dfc75bbc655a277c4cf90cd4d38d13febaad
n0y0j <n0y0j77@gmail.com>
2021-02-13 18:43:54 +0900

    feat: Implement Navbar ui.

--- travel_web/src/App.js
+++ travel_web/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Yo</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Router path='/' exact />
+        </Switch>
+      </Router>
+    </>
   )
 }
 
