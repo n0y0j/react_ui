d19047b0c7fd446ed80f66f17fc9a5971133e209
n0y0j <n0y0j77@gmail.com>
2021-02-16 15:45:12 +0900

    feat: Implement Button design.

---
+++ card_issue_web/src/components/Button.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Button.css";
+
+const STYLES = ["btn--primary", "btn--outline"];
+
+const SIZES = ["btn--medium", "btn--large", "btn--mobile", "btn--wide"];
+
+const COLOR = ["primary", "blue", "red", "green"];
+
+export const Button = ({
+  children,
+  type,
+  onClick,
+  buttonStyle,
+  buttonSize,
+  buttonColor,
+}) => {
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
+
+  const checkButtonSized = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+
+  const checkButtonColor = SIZES.includes(buttonColor) ? buttonColor : COLOR[0];
+
+  return (
+    <button
+      className={`btn ${checkButtonStyle} ${checkButtonSized} ${checkButtonColor}`}
+      onClick={onClick}
+      type={type}
+    >
+      {children}
+    </button>
+  );
+};
