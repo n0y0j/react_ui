b901c536863fee9933d78ce94cf42f6f4fcdfe9a
n0y0j <n0y0j77@gmail.com>
2021-02-18 13:38:45 +0900

    feat: Implement footer.

--- card_issue_web/src/App.js
+++ card_issue_web/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/pages/Footer/Footer';
 import Home from './components/pages/HomePage/Home';
 
 
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route path='/' exact component={Home}/>
       </Switch>
+      <Footer />
     </Router>
   );
 }
