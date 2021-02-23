f26100303b131c73a3f4c3699bc64d8445b423fa
n0y0j <n0y0j77@gmail.com>
2021-02-23 20:04:32 +0900

    feat: Implement trip card section.

--- travel_web_v2/gatsby-config.js
+++ travel_web_v2/gatsby-config.js
@@ -11,7 +11,14 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `video`,
+        path: `${__dirname}/src/assets/videos`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -26,6 +33,13 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./src/data/`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- travel_web_v2/src/pages/index.js
+++ travel_web_v2/src/pages/index.js
@@ -1,14 +1,15 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hero from "../components/Hero"
+import Trips from "../components/Trips"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Hero />
+    <Trips heading="Our Favorite Destinations" />
   </Layout>
 )
 
