4f4094d4ad943d1bf2baefbd2c5805e501466f02
n0y0j <n0y0j77@gmail.com>
2021-02-23 15:53:31 +0900

    feat: Implement top navigation bar.

--- travel_web_v2/src/components/layout.js
+++ travel_web_v2/src/components/layout.js
@@ -1,9 +1,11 @@
 import React from "react"
 import Header from "./Header"
+import {GlobalStyle} from "./styles/GlobalStyles"
 
 const Layout = ({ children }) => {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <main>{children}</main>
     </>
