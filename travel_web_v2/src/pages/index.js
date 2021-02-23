14163586778a9d8cc72ec46c762b98803330ec01
n0y0j <n0y0j77@gmail.com>
2021-02-23 17:02:24 +0900

    feat: Implement hero section.

--- travel_web_v2/src/pages/index.js
+++ travel_web_v2/src/pages/index.js
@@ -3,11 +3,12 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Hero from "../components/Hero"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <Hero />
   </Layout>
 )
 
